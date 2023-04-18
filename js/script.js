1668b6ccdb2d80e0a7f3d5274f5788c4228e465e
Maciej Martynowski <MaciejWork2022@gmail.com>
2023-04-18 22:05:03 +0200

    Functions added

--- js/script.js
+++ js/script.js
@@ -1,34 +1,48 @@
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
-let currencyElement = document.querySelector(".js-currency");
-let amountElement = document.querySelector(".js-amount");
-let euroElement = document.querySelector(".js-euro");
-let dolarElement = document.querySelector(".js-dolar");
-let funtElement = document.querySelector(".js-funt");
+{
+    const calculate = (event) => {
+        event.preventDefault();
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+        const euroElement = document.querySelector(".js-euro");
+        const dolarElement = document.querySelector(".js-dolar");
+        const funtElement = document.querySelector(".js-funt");
+        const amountElement = document.querySelector(".js-amount");
 
-let currencyRate;
-let currencyName;
+        let currencyRate;
+        let currencyName;
 
-switch(true) {
-    case euroElement.checked:
-        currencyRate = 4.68;
-        currencyName = 'euro';
-        break;
-    case dolarElement.checked:
-        currencyRate = 4.32;
-        currencyName = 'dolara';
-        break;
-    default:
-        currencyRate = 5.32;
-        currencyName = 'funta';
+        switch (true) {
+            case euroElement.checked:
+                currencyRate = 4.68;
+                currencyName = 'euro';
+                break;
+            case dolarElement.checked:
+                currencyRate = 4.32;
+                currencyName = 'dolara';
+                break;
+            case funtElement.checked:
+                currencyRate = 5.32;
+                currencyName = 'funta';
+                break;
+        }
+
+        const result = amountElement.value / currencyRate;
+
+        resultText(result, currencyName, currencyRate);
+    }
+
+    const resultText = (result, currencyName, currencyRate) => {
+        const resultElement = document.querySelector(".js-result");
+        const currencyElement = document.querySelector(".js-currency");
+
+        resultElement.innerText = result.toFixed(2);
+        currencyElement.innerText = `Kurs ${currencyName} wynosi ${currencyRate}.`;
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", calculate);
+    }
+
+    init();
 }
-
-result = amountElement.value / currencyRate;
-
-resultElement.innerText = result.toFixed(2);
-currencyElement.innerText = `Kurs ${currencyName} wynosi ${currencyRate}.`;
-
-})
